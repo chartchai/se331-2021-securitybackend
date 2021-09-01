088375d51d2ef00a426d73aa8abb10018578b7e8
Chartchai <chartchai.d@cmu.ac.th>
2021-09-01 11:27:21 +0700

    add all data mapping

--- src/main/java/se331/lab/rest/entity/Event.java
+++ src/main/java/se331/lab/rest/entity/Event.java
@@ -1,11 +1,20 @@
 package se331.lab.rest.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
+    @Id
     Long id;
     String category;
     String title;
