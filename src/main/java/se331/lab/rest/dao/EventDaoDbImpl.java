76b974184e2600af65cf49c9940bee8cf47bbd1e
Chartchai <chartchai.d@cmu.ac.th>
2021-09-01 14:10:36 +0700

    Change to pagable

--- src/main/java/se331/lab/rest/dao/EventDaoDbImpl.java
+++ src/main/java/se331/lab/rest/dao/EventDaoDbImpl.java
@@ -2,31 +2,28 @@ package se331.lab.rest.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Repository;
 import se331.lab.rest.entity.Event;
 import se331.lab.rest.repository.EventRepository;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Repository
 @Profile("db")
-public class EventDaoDbImpl implements EventDao{
+public class EventDaoDbImpl implements EventDao {
     @Autowired
     EventRepository eventRepository;
+
     @Override
     public Integer getEventSize() {
         return Math.toIntExact(eventRepository.count());
     }
 
     @Override
-    public List<Event> getEvents(Integer pageSize, Integer page) {
-        List<Event> events = eventRepository.findAll();
-        pageSize = pageSize == null ? events.size() : pageSize;
-        page = page == null ? 1 : page;
-        int firstIndex = (page - 1) * pageSize;
-        List<Event> output = events.subList(firstIndex, firstIndex + pageSize);
-        return output;
+    public Page<Event> getEvents(Integer pageSize, Integer page) {
+        return eventRepository.findAll(PageRequest.of(page - 1, pageSize));
     }
 
     @Override
