60aad7082b843c08b739bdb8a9ab4b79fb9b522c
Chartchai <chartchai.d@cmu.ac.th>
2021-09-01 13:44:31 +0700

    Add Data to db, then try to connect the data

--- src/main/java/se331/lab/rest/entity/Event.java
+++ src/main/java/se331/lab/rest/entity/Event.java
@@ -1,11 +1,10 @@
 package se331.lab.rest.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -15,6 +14,8 @@ import javax.persistence.Id;
 @AllArgsConstructor
 public class Event {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
     Long id;
     String category;
     String title;
