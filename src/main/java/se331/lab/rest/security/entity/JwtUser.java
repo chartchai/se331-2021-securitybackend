0f34487fc5c9cacfba1b8fad9c4f72dc479e1078
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2021-09-27 14:00:07 +0700

    add new security setup files in config/SecurityConfig and security folder

---
+++ src/main/java/se331/lab/rest/security/entity/JwtUser.java
@@ -0,0 +1,92 @@
+package se331.lab.rest.security.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtUser implements UserDetails {
+
+    Long id;
+    String username;
+    String firstname;
+    String lastname;
+    String password;
+    String email;
+    Collection<? extends GrantedAuthority> authorities;
+    boolean enabled;
+    Date lastPasswordResetDate;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @JsonIgnore
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    @JsonIgnore
+    public Date getLastPasswordResetDate() {
+        return lastPasswordResetDate;
+    }
+}
